5a3e67aaa7286d804fc20a3f6da41fb7297b15e4
memmokx <134604022+memmokx@users.noreply.github.com>
2025-11-02 17:20:50 +0100

    feat(fssl): fix base64 size macros

--- include/fssl/encoding.h
+++ include/fssl/encoding.h
@@ -8,13 +8,7 @@
 #define fssl_hex_encoded_size(size) ((size) * 2)
 #define fssl_hex_decoded_size(size) ((size) / 2)
 
-#define fssl_base64_encoded_size(size)      \
-  ({                                        \
-    auto _size = (size);                    \
-    auto _mod = _size % 4;                  \
-    _mod != 0 ? (_size + 4 - _mod) : _size; \
-  })
-
+#define fssl_base64_encoded_size(size) (((size)) / 3 * 4)
 #define fssl_base64_decoded_size(size) ((size) / 4 * 3)
 
 fssl_error_t fssl_hex_encode(const uint8_t* data, size_t len, char* buf, size_t buf_capacity);
